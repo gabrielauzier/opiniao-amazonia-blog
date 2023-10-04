3fc6578bbc7eafb17900c41e36e612420721bf6b
gabrielauzier <gabrielvascoauzier@gmail.com>
2023-10-04 16:52:34 -0400

    feat: add posts list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
-import { InDevelopment } from './in-development'
+import { Input } from '@/presentation/components/Form/Input'
+import { PostPreview } from '@/presentation/components/Posts/PostPreview'
+import { Search } from 'lucide-react'
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-3xl font-medium text-zinc-900">Home</h1>
-      <InDevelopment />
-    </>
+    <div>
+      <Input.Root>
+        <Input.Prefix>
+          <Search />
+        </Input.Prefix>
+        <Input.Control placeholder="Buscar conteúdo" />
+      </Input.Root>
+      <div className="mt-16 pb-8">
+        <h1 className="mb-5 text-3xl font-bold">Últimas postagens</h1>
+
+        <div className="flex flex-col gap-8 divide-y-2">
+          <PostPreview />
+          <PostPreview />
+          <PostPreview />
+          <PostPreview />
+          <PostPreview />
+          <PostPreview />
+          <PostPreview />
+          <PostPreview />
+        </div>
+      </div>
+    </div>
   )
 }

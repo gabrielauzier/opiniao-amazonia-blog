549065cf38743d3df0f5fdf4d1c57815c84bf030
gabrielauzier <gabrielvascoauzier@gmail.com>
2023-11-02 19:35:53 -0400

    feat: change nav bar behaviour when off the home page

--- src/presentation/components/Header/Navigation/index.tsx
+++ src/presentation/components/Header/Navigation/index.tsx
@@ -2,14 +2,18 @@ import { twMerge } from 'tailwind-merge'
 
 interface NavBarProps {
   isBackgroundTransparent: boolean
+  currentPath: string
 }
 
-export function NavBar({ isBackgroundTransparent }: NavBarProps) {
+export function NavBar({ isBackgroundTransparent, currentPath }: NavBarProps) {
   return (
     <nav
       className={twMerge(
         'flex gap-8 text-sm 2xl:text-base',
-        isBackgroundTransparent ? 'text-white' : 'text-zinc-700',
+        isBackgroundTransparent && currentPath === '/'
+          ? 'text-zinc-200'
+          : 'text-zinc-700',
+        currentPath !== '/' && 'text-zinc-200',
       )}
     >
       <span>DESTAQUES</span>
--- src/presentation/components/Header/index.tsx
+++ src/presentation/components/Header/index.tsx
@@ -4,10 +4,11 @@ import { Search } from 'lucide-react'
 import { NavBar } from './Navigation'
 import { useEffect, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
-import { useWindowDimensions } from '@/presentation/hooks/use-window-dimensions'
+import { usePathname } from 'next/navigation'
 
 export function Header() {
   const [isNavbarTransparent, setIsNavbarTransparent] = useState(true)
+  const currentPath = usePathname()
 
   function handleNavbarBackgroundChanging() {
     if (window.scrollY >= 80) {
@@ -25,18 +26,20 @@ export function Header() {
     <header
       className={twMerge(
         'fixed top-0 z-50 hidden h-header w-full lg:block',
-        !isNavbarTransparent
+        !isNavbarTransparent && currentPath === '/'
           ? 'animate-slideDownAndFade bg-white shadow-md'
           : 'bg-transparent',
+        currentPath !== '/' &&
+          'animate-slideDownAndFade bg-zinc-900 text-zinc-200',
       )}
     >
       <div
         className={twMerge(
-          'mx-auto flex h-header max-w-header items-center justify-between bg-transparent 2xl:text-xl',
-          isNavbarTransparent ? 'text-white' : 'text-zinc-700',
+          'mx-auto flex h-header max-w-header items-center justify-between bg-transparent text-zinc-200 2xl:text-xl',
+          !isNavbarTransparent && currentPath === '/' && 'text-zinc-700',
+          currentPath !== '/' && 'text-zinc-200',
         )}
       >
-        {/* <h1 className="font-serif text-3xl font-semibold">Opinião Amazônia</h1> */}
         <h1
           className={twMerge(
             'bg-gradient-to-r from-emerald-700 to-emerald-500 bg-clip-text font-serif text-3xl font-semibold text-transparent',
@@ -45,11 +48,21 @@ export function Header() {
           Opinião Amazônia
         </h1>
 
-        <NavBar isBackgroundTransparent={isNavbarTransparent} />
+        <NavBar
+          isBackgroundTransparent={isNavbarTransparent}
+          currentPath={currentPath}
+        />
 
         <div className="flex items-center gap-6">
           <Search />
-          <button className="rounded-sm bg-black px-10 py-2 text-white shadow-sm">
+          <button
+            className={twMerge(
+              'rounded-sm bg-black px-10 py-2 text-base shadow-sm',
+              currentPath === '/'
+                ? 'bg-zinc-900 text-zinc-100'
+                : 'bg-zinc-100 text-zinc-700',
+            )}
+          >
             Entrar
           </button>
         </div>

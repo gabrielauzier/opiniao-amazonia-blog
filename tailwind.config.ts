c6db6e1836161329556dfea6b0c446b5fc79d6dd
gabrielauzier <gabrielvascoauzier@gmail.com>
2023-11-11 19:11:03 -0400

    feat: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,22 +6,15 @@ const config: Config = {
     extend: {
       fontSize: {
         xxs: '10px',
-      },
-      gridTemplateColumns: {
-        // app: 'minmax(18rem, 20rem) 1fr',
-        profile: 'max-content 1fr min-content',
-        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 1fr)',
-        header: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 1fr)',
-        body: '1fr minmax(18rem, 20rem) ',
-        app: 'minmax(18rem, 36rem) 1fr',
-        post: '1fr 12rem',
-        blog: '512px 1fr',
+        xs: '12px',
       },
 
       backgroundImage: {
         hero: "linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.05)), url('/rio.jpg')",
         contact:
           "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.05)), url('/manaus4.jpg')",
+        newsletter:
+          "linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.5)), url('/newsletter.jpg')",
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
@@ -37,20 +30,13 @@ const config: Config = {
           "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.05)), url('https://images.pexels.com/photos/7903925/pexels-photo-7903925.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
       },
 
-      borderWidth: {
-        6: '6px',
-      },
-
-      colors: {
-        brand: '#EE5626',
-        'brand-hover': '#FE8C68',
-        'brand-light': '#FFE6DE',
-        'brand-extra-light': '#D4D4D8',
-      },
-
       boxShadow: {
         '3xl': '0 0 10px rgba(0, 0, 0, 0.25)',
         '4xl': '0 0 65px rgba(0, 0, 0, 0.15)',
+      },
+
+      colors: {
+        'surface-seconday': '#F8F8F8',
       },
 
       maxWidth: {
@@ -65,6 +51,10 @@ const config: Config = {
       height: {
         body: 'calc(100vh - 3.5rem)',
         header: '5rem',
+      },
+
+      marginTop: {
+        999: '5rem',
       },
 
       padding: {
@@ -97,6 +87,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
 }
+
 export default config

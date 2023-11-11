2891bf6299b2424624bd7a60e32c57d70dc2a433
gabrielauzier <gabrielvascoauzier@gmail.com>
2023-11-11 19:16:59 -0400

    feat: update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import '@/presentation/styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Header } from '@/presentation/components/Header'
+import { App } from './app'
+import { Footer } from '@/presentation/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,13 +18,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="overflow-x-hidden">
-      <body className={inter.className}>
-        <div className="relative h-[100vh] w-[100vw]">
-          <Header />
-          <main>{children}</main>
-        </div>
-      </body>
-    </html>
+    <App>
+      <html lang="en" className="overflow-x-hidden">
+        <body className={inter.className}>
+          <div className="relative h-[100vh] w-[100vw]">
+            <Header />
+            <main>{children}</main>
+            <Footer />
+          </div>
+        </body>
+      </html>
+    </App>
   )
 }

01e3a4537a4b027b8f165c9b9eaa15f4fcfec998
gabrielauzier <gabrielvascoauzier@gmail.com>
2023-11-11 19:12:40 -0400

    feat: add home sections

--- src/presentation/pages/home/sections/Banner.tsx
+++ src/presentation/pages/home/sections/Banner.tsx
@@ -1,26 +1,31 @@
 import { ArrowRight } from 'lucide-react'
 import { twMerge } from 'tailwind-merge'
 
+import Link from 'next/link'
+
 export function Banner() {
   return (
     <section
       className={twMerge(
-        'hidden w-full flex-col items-center justify-center gap-1 bg-cover text-center font-normal shadow-xl md:bg-hero lg:flex',
+        'flex w-full flex-col items-center justify-center gap-1 bg-hero bg-cover text-center font-normal shadow-xl',
         'h-[32rem]',
       )}
     >
-      <h2 className="px-5 text-2xl text-white lg:text-6xl">
+      <h2 className="px-5 text-4xl text-white lg:text-6xl">
         NAVEGUE PELA SELVA DAS IDEIAS
       </h2>
-      <h2 className="text-white lg:text-4xl">
+      <h2 className="text-xl text-white lg:text-4xl">
         O Palco do Debate Político Amazônico!
       </h2>
-      <button className="group mt-10 flex w-fit items-center gap-2 rounded-sm bg-white px-10 py-4 text-black shadow-md duration-300 ease-in-out hover:scale-110">
+      <Link
+        className="group mt-10 flex w-fit cursor-pointer items-center gap-2 rounded-sm bg-white px-10 py-4 text-black shadow-md duration-300 ease-in-out hover:scale-110"
+        href="/posts"
+      >
         Conheça nosso blog
         <span className="rounded-full bg-black p-1 duration-300 ease-in-out group-hover:bg-emerald-500">
           <ArrowRight className="h-4 w-4 text-white" />
         </span>
-      </button>
+      </Link>
     </section>
   )
 }
--- src/presentation/pages/home/sections/PostList.tsx
+++ src/presentation/pages/home/sections/PostList.tsx
@@ -1,6 +1,11 @@
+import { PostModel } from '@/domain/models'
 import { PostPreview } from '@/presentation/pages/posts/PostPreview'
 
-export function PostList() {
+interface PostListProps {
+  posts: PostModel[]
+}
+
+export function PostList({ posts }: PostListProps) {
   return (
     <section className="mx-auto mt-0 max-w-[28rem]   px-5 lg:max-w-body lg:px-0">
       <div className="mb-5">
@@ -11,12 +16,24 @@ export function PostList() {
       </div>
 
       <div className="grid gap-10 lg:grid-cols-3">
+        {posts.map((post) => (
+          <PostPreview
+            key={post.id}
+            title={post.title}
+            date={post.date}
+            excerpt={post.excerpt}
+            categories={post.categories}
+            imgUrl={post.imgUrl}
+            slug={post.slug}
+          />
+        ))}
         <PostPreview
           title="Integer Maecenas Eget Viverra"
           date="28 de julho, 2023"
           excerpt="Aenean eleifend ante maecenas pulvinar montes lorem et pede dis dolor pretium donec dictum. Vici consequat justo enim. Venenatis eget adipiscing luctus lorem."
           categories={['Política', 'Cultura']}
           imgUrl="https://images.pexels.com/photos/1550337/pexels-photo-1550337.jpeg"
+          slug="title-1"
         />
         <PostPreview
           title="Aenean eleifend ante maecenas"
@@ -24,6 +41,7 @@ export function PostList() {
           excerpt="Aenean eleifend ante maecenas pulvinar montes lorem et pede dis dolor pretium donec dictum. Vici consequat justo enim. Venenatis eget adipiscing luctus lorem."
           categories={['Política', 'Cultura']}
           imgUrl="https://images.pexels.com/photos/3760067/pexels-photo-3760067.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          slug="title-2"
         />
         <PostPreview
           title="Integer Maecenas Eget Viverra"
@@ -31,6 +49,7 @@ export function PostList() {
           excerpt="Aenean eleifend ante maecenas pulvinar montes lorem et pede dis dolor pretium donec dictum. Vici consequat justo enim. Venenatis eget adipiscing luctus lorem."
           categories={['Política', 'Brasília']}
           imgUrl="https://images.pexels.com/photos/5721097/pexels-photo-5721097.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          slug="title-3"
         />
         <PostPreview
           title="Integer Maecenas Eget Viverra"
@@ -38,6 +57,7 @@ export function PostList() {
           excerpt="Aenean eleifend ante maecenas pulvinar montes lorem et pede dis dolor pretium donec dictum. Vici consequat justo enim. Venenatis eget adipiscing luctus lorem."
           categories={['Manaus', 'Cultura']}
           imgUrl="https://images.pexels.com/photos/7903925/pexels-photo-7903925.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          slug="title-4"
         />
         <PostPreview
           title="Integer Maecenas Eget Viverra"
@@ -45,6 +65,7 @@ export function PostList() {
           excerpt="Aenean eleifend ante maecenas pulvinar montes lorem et pede dis dolor pretium donec dictum. Vici consequat justo enim. Venenatis eget adipiscing luctus lorem."
           categories={['Política', 'Cultura']}
           imgUrl="https://images.pexels.com/photos/13809881/pexels-photo-13809881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          slug="title-5"
         />
         <PostPreview
           title="Integer Maecenas Eget Viverra"
@@ -52,6 +73,7 @@ export function PostList() {
           excerpt="Aenean eleifend ante maecenas pulvinar montes lorem et pede dis dolor pretium donec dictum. Vici consequat justo enim. Venenatis eget adipiscing luctus lorem."
           categories={['Política', 'Cultura']}
           imgUrl="https://images.pexels.com/photos/1708988/pexels-photo-1708988.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          slug="title-6"
         />
       </div>
 

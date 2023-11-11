50111050f9ebed7185c18c9379c816eefbe83282
gabrielauzier <gabrielvascoauzier@gmail.com>
2023-11-11 19:09:17 -0400

    refactor: remove useless files

--- src/presentation/components/Aside/Tags/Root.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from 'react'
-
-interface RootProps {
-  children: ReactElement[]
-}
-
-export function Root({ children }: RootProps) {
-  return <div className="flex flex-wrap gap-2.5">{children}</div>
-}
